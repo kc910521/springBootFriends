b01a39c03f33b56b4c16924191a3d1a55532e39f
kc910521 <kc910521@qq.com>
2018-03-05 16:13:36 +0800

    fst commit 4 easy sample

---
+++ src/main/java/ind/config/SecurityConfig.java
@@ -0,0 +1,45 @@
+package ind.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+/**
+ * @author BJQXDN0626
+ * @create 2018/3/5
+ */
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers("/css/**", "/index").permitAll()
+                .antMatchers("/user/**").hasRole("USER")
+                .and()
+                .formLogin()
+                .loginPage("/login").failureUrl("/login-error");
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .inMemoryAuthentication()
+                .passwordEncoder(new PasswordEncoder() {
+                    @Override
+                    public String encode(CharSequence charSequence) {
+                        return charSequence.toString();
+                    }
+
+                    @Override
+                    public boolean matches(CharSequence charSequence, String s) {
+                        return s.equals(charSequence.toString());
+                    }
+                })
+                .withUser("user").password("password").roles("USER");
+    }
+}
--- src/main/java/ind/controller/TestController.java
+++ src/main/java/ind/controller/TestController.java
@@ -1,5 +1,7 @@
 package ind.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,12 +9,32 @@ import org.springframework.web.bind.annotation.RestController;
  * @author BJQXDN0626
  * @create 2017/12/11
  */
-@RestController
+@Controller
 public class TestController {
 
     @RequestMapping("/")
-    public String index() {
+    public String root() {
+        return "redirect:/index";
+    }
 
-        return "Greetings from Spring Boot!";
+    @RequestMapping("/index")
+    public String index() {
+        return "index";
+    }
+
+    @RequestMapping("/user/index")
+    public String userIndex() {
+        return "user/index";
+    }
+
+    @RequestMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @RequestMapping("/login-error")
+    public String loginError(Model model) {
+        model.addAttribute("loginError", true);
+        return "login";
     }
 }
